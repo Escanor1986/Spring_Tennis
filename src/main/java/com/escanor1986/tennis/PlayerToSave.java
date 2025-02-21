04fa5cff317faa85c1e131aa58925c2916e97d60
Escanor1986 <lionel.zovi@gmail.com>
2025-02-21 14:38:12 +0100

    fix(mapping): corriger l'inversion des attributs entre Player et PlayerToSave
    - Correction de l'ordre des attributs dans Player et PlayerToSave pour garantir que le mapping respecte l'ordre attendu.
    - Mise à jour des getters et constructeurs afin que les données retournées correspondent aux attentes des tests unitaires et de l'API (Swagger/curl).

--- src/main/java/com/escanor1986/tennis/PlayerToSave.java
+++ src/main/java/com/escanor1986/tennis/PlayerToSave.java
@@ -8,8 +8,8 @@ import jakarta.validation.constraints.PastOrPresent;
 import jakarta.validation.constraints.PositiveOrZero;
 
 public record PlayerToSave(
-        @NotBlank(message = "First name is mandatory") String firstName,
         @NotBlank(message = "Last name is mandatory") String lastName,
+        @NotBlank(message = "First name is mandatory") String firstName,
         @NotNull(message = "Birth date is mandatory") @PastOrPresent(message = "Birth date must be in the past or present") LocalDate birthDate,
         @PositiveOrZero(message = "Points must be positive or zero") Integer points) {
 }
